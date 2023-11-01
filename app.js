3fbb21dabbb2163ae1eb0cd64c2b9aef6eda72d3
Charlotte <charlie.stone649@gmail.com>
2023-11-01 19:34:30 +0000

    further changes to the app.js file

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
-import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter'
+import { RecursiveCharacterTextSplitter } from '/langchain/text_splitter';
 
 try {
-  const result = await fetch('Yakuzi.txt')
-  const text = await result.text()
+  const result = await fetch('Yakuzi.txt');
+  const text = await result.text();
+
+  const splitter = new RecursiveCharacterTextSplitter({
+    chunkSize: 500,
+    separators: ['\n\n', '\n', ' ', ''],
+    chunkOverlap: 50
+  })
+
+  const output = await splitter.createDocuments([text]);
+  console.log(output);
 } catch (err) {
-  console.log(err)
+  console.log(err);
 }
